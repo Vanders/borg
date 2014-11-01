6391792176939f335b6abcd4f41678491702b05b
Alex Howells <alex@howells.me>
2014-11-01 21:09:30 +0000

    'dem Rubocop fixes. Gotta keep the big metal man happy.

--- config/borg.rb
+++ config/borg.rb
@@ -7,19 +7,19 @@ cookbook_path [
   "#{current_dir}/cookbooks/upstream"
 ]
 
-data_bag_path    "#{current_dir}/data_bags"
+data_bag_path "#{current_dir}/data_bags"
 environment_path "#{current_dir}/environments"
-role_path        "#{current_dir}/roles"
-json_attribs     "#{current_dir}/config/dna.json"
+role_path "#{current_dir}/roles"
+json_attribs "#{current_dir}/config/dna.json"
 
-log_max_age      3
-log_level        :info
-log_location     "#{borg_homedir}/logs/chef-client-#{Time.now.to_i}.log"
-verbose_logging  true
+log_max_age 3
+log_level :info
+log_location "#{borg_homedir}/logs/chef-client-#{Time.now.to_i}.log"
+verbose_logging true
 
-ssl_verify_mode  :verify_peer
-verify_api_cert  true
+ssl_verify_mode :verify_peer
+verify_api_cert true
 
-cache_type       "BasicFile"
-file_cache_path	 "#{borg_homedir}/cache"
+cache_type 'BasicFile'
+file_cache_path "#{borg_homedir}/cache"
 file_backup_path "#{borg_homedir}/backups"
