c5783db58cab8f4017845c4454123c3bf3c036c5
Alex Howells <alex@howells.me>
2014-11-01 18:07:27 +0000

    Extremely simplistic method to update Borg on a system

---
+++ cookbooks/project/borg/recipes/update.rb
@@ -0,0 +1,22 @@
+node.default["borg"]["repository"] = "https://github.com/agh/borg"
+
+case node.current_user
+when "root"
+  node.default["borg"]["root"] = "/opt/borg"
+  node.default["borg"]["source_dir"] = "#{node.borg.root}/src"
+else
+  node.default["borg"]["root"] = "#{node.etc.passwd[node.current_user].dir}/.borg"
+  node.default["borg"]["source_dir"] = "#{node.borg.root}/src"
+end
+
+directory node.borg.source_dir do
+  owner node.current_user
+  group node.current_user
+  mode '0700'
+end
+
+git "borg" do
+  repository node.borg.repository
+  revision "master"
+  action :sync
+end
