9fb6a8cd33c0efb038f7c749c704029cfccd8487
Alex Howells <alex@howells.me>
2014-11-02 23:20:27 +0000

    Add a probably functional install script

--- README.md
+++ README.md
@@ -3,38 +3,36 @@
 ![cube](http://f.cl.ly/items/1U1z2f0J060L0Q2v111h/borg-cube-angled.jpg)
 
 I've always found it a ginormous pain to get an OSX system configured
-_"just right"_ in any *reasonable* amount of time, or to keep multiple systems
-vaguely in sync in terms of the software and settings deployed on them.
+_"just right"_ in any **reasonable** amount of time, or to keep multiple systems
+vaguely in sync in terms of the software and settings.
 
 I'd guess this is a pain I feel a couple of times a year on average. It sucks.
-It's like the difference between sleeping in your own bed, or a :hotel: bed,
-you just feel :tired_face: using the system if it isn't built right.
 
-The goal is thus: Build a system from scratch to >99% done in <60 minutes.
-Manual steps and checklists are okay, because Apple doesn't make it easy
-to access the App Store from the CLI, but automate as much as sensible.
-Anything I have to do manually will be documented in [agh/setup](https://github.com/agh/setup).
+Manual steps and checklists are okay, and Borg automates as much as sensible.
+Everything involved in setting up my systems is over in [agh/setup](https://github.com/agh/setup)
+and that includes pointy-clicky actions taken in Mac App Store, et al.
 
-Where possible settings are parameterized, so you can use this too without :rage3:,
-and Borg is chef-solo so it should be fast to get going. No need for Chef Server.
+Where possible settings are parameterized, so you can use this too without :rage3:
+and Borg is chef-solo so it has few external dependencies. No need for Chef Server.
 
 The intent is that Borg should run as your user (e.g. `agh`) and that it will
-elevate privileges :muscle: with sudo as required to install packages, etc.
+elevate privileges with sudo :muscle: as required to install packages, etc.
 Something like 95% of the lifting is done by Chef, the remaining 5% is Bash,
 which maintains XCode Command Line Tools, Homebrew, et al.
 
 None of the fancy :zap: features in Bash 4.x can be used until Apple ships that.
-No warranty is supplied generally, but if you try to run this on Ruby 1.9 
-(or earlier) since OSX 10.10 ('Yosemite') now ships Ruby 2.0.0-p481,
-then you are *especially* unsupported :fire: (and slightly crazy - upgrade!).
+No warranty is supplied. If you try to run this on Ruby 1.9 (or earlier) or on
+anything before OSX 10.10 ('Yosemite') then your results **will** vary. Sorry.
+Perhaps you should give consideration to upgrading?
 
 ## Prerequisites
 
   * Install OSX 'Yosemite' (10.10) from scratch onto your system.
+  * Be the only user of that system. This takes over /usr/local and other places.
 
 ## Install
 
-  * `curl -L https://raw.github.com/agh/borg/master/script/install | bash`
+  * `curl -L https://raw.github.com/agh/borg/master/script/install.sh | bash`
 
 ## Operation
 
---
+++ script/install.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+#
+#      ___________
+#     /-/_'/-/_/-/|                   B   O   R   G
+#    /'-/-_'/-_//||          << https://github.com/agh/borg >>
+#   /__________/|/|
+#   |'|_'='-]:+|/||   Sit back and relax, human. Resistance is futile.
+#   |-+-|.|_'-'||//   This should be a painless process and before long this
+#   |['.[:!+-'=|//    system will be a functional part of the collective.
+#   |='!+|-:]|-|/
+#    ----------
+#
+#  Author: Alex Howells <alex@howells.me>
+#  Copyright (c) 2014, Aeode Ltd <hello@aeode.com>
+#  Please see the LICENSE file in the repository root for further information.
+#
+function bold() {
+  bold=`tput bold`
+  normal=`tput sgr0`
+  echo "${bold}$1${normal}"
+}
+
+function notify() {
+  echo "==> $1"
+}
+
+function subnotify() {
+  echo "     $1"
+}
+
+cat << __EOF__
+
+$(tput setaf 4)    ___________                                                           
+$(tput setaf 4)   /-/_'/-/_/-/| $(tput setaf 1)                  B   O   R   G                            
+$(tput setaf 4)  /'-/-_'/-_//|| $(tput setaf 1)         << https://github.com/agh/borg >>                
+$(tput setaf 4) /__________/|/| $(tput setaf 2)                                                           
+$(tput setaf 4) |'|_'='-]:+|/|| $(tput setaf 2)  Sit back and relax, human. Resistance is futile.         
+$(tput setaf 4) |-+-|.|_'-'||// $(tput setaf 2)  This should be a painless process and before long this   
+$(tput setaf 4) |['.[:!+-'=|//  $(tput setaf 2)  system will be a functional part of the collective.      
+$(tput setaf 4) |='!+|-:]|-|/
+$(tput setaf 4)  ----------
+$(tput sgr0)
+__EOF__
+
+bold "  Ready to get started? We'll have you ready to assimilate in just a tick."
+bold "  We need your password so packages that require sudo can be installed."
+  
+# Ensure that sudo isn't going to prompt for a password in a weird place.
+sudo -k && sudo -p "  Password: " -l 2>&1 >/dev/null && echo "" || exit 1
+
+#
+# Xcode Command Line Tools
+#
+if [ ! $(xcode-select -p) ]; then
+  notify 'Installing the Xcode Command Line Tools. Press <ENTER> when this completes.'
+  xcode-select --install
+  read
+else
+  notify '👍  Found the Xcode Command Line Tools!'
+fi
+
+#
+# ChefDK
+#
+chefdk_version="0.3.2-1"
+
+(test -x /opt/chefdk/bin/chef-solo && 
+  (/opt/chefdk/bin/chef --version | grep 0.3.2 2>&1 >/dev/null &&
+    (notify "👍  Found the Chef Development Kit!"))) || {
+
+    # If we got this far, then we need to install or upgrade ChefDK.
+    notify "Installing the Chef Development Kit (${chefdk_version})"
+
+    # This naively assumes we are running on OSX.
+    platform="mac_os_x"
+    release="10.8"
+
+    # Perform the download.
+    fname="chefdk-${chefdk_version}.dmg"
+    url="https://opscode-omnibus-packages.s3.amazonaws.com/$platform/$release/x86_64/$fname"
+    subnotify "Downloading ${url}..."
+    cd /tmp && curl -L -O $url
+
+    # Perform the install
+    test "$platform" = "mac_os_x" && {
+      hdiutil attach /tmp/$fname 2>&1 >/dev/null && echo "mounted"
+      sudo installer -package "/Volumes/Chef Development Kit/chefdk-${chefdk_version}.pkg" -target /
+      hdiutil detach "/Volumes/Chef Development Kit" 2>&1 >/dev/null && rm /tmp/$fname
+    }
+}
+
+#
+# Homebrew
+#
+if [ ! -d /usr/local/.git ]; then
+  notify "🚄  Doesn't look like you have Homebrew. I'll clone that down for you!"
+  sudo rm -rf /usr/local
+  sudo git clone https://github.com/mxcl/homebrew /usr/local
+else
+  notify "👍  Looks like you already have Homebrew installed!"
+fi
+
+if [ ! -d /opt/homebrew-cask ]; then
+  notify "👎  I can't find the necessary directories for Homebrew. Gonna help you create them."
+  sudo mkdir -p /Library/Caches/Homebrew /opt/homebrew-cask
+else
+  notify "👍  Looks like all the extra directories for Homebrew exist!"
+fi
+
+if [ $(stat -f "%Su" /usr/local) != "$USER" ]; then
+  notify "😠  Permissions on /usr/local look wrong. Gonna help you fix that."
+  sudo chown -R $USER:admin /usr/local /Library/Caches/Homebrew /opt/homebrew-cask
+else
+  notify "👍  Permissions on necessary directories all look good!"
+fi
+
+#
+# Borg
+#
+if [ ! -d $HOME/.borg ]; then
+  notify "👎  Looks like we need to clone the rest of Borg. I'll do that now!"
+ 
+  BORG_ROOT="${HOME}/.borg"
+
+  # Let's make some directories to get the basics installed.
+  BORG_DIRECTORIES="logs src"
+  for directory in $BORG_DIRECTORIES; do
+    mkdir -p $BORG_ROOT/$directory
+  done
+
+  # Pull down source from GitHub for the first time.
+  git clone https://github.com/agh/borg $BORG_ROOT/src
+
+  # Symlinks so that stuff appears in PATH.
+  BORG_TOOLS="borg"
+  for tool in $BORG_TOOLS; do
+    if [ ! -f /usr/local/bin/$tool ]; then
+      ln $BORG_ROOT/src/script/$tool /usr/local/bin/$tool
+    fi
+  done
+else
+  notify "👍  I've found Borg and thus you can 'borg update' to get newer code."
+fi
+
+#
+# Validation
+# Also known as 'checks and balances', or making sure everything worked!
+#
+type chef-solo >/dev/null 2>&1 || {
+  echo >&2 "Expected to find chef-solo but it is missing. Bummer!"
+  exit 1
+}
+
+type berks >/dev/null 2>&1 || {
+  echo >&2 "Expected to find Berkshelf but it is missing. Bummer!"
+  exit 1
+}
+
+if [ $(stat -f "%Su" /usr/local) != "$USER" ]; then
+  echo >&2 "Expected your user to own /usr/local, and you need to fix this!"
+  exit 1
+fi
+
+if [ ! -d /opt/homebrew-cask ] || [ $(stat -f "%Su" /opt/homebrew-cask) != "$USER" ]; then
+  echo >&2 "Expected your user to own /opt/homebrew-cask, and you need to fix this!"
+  exit 1
+fi
+
+type borg >/dev/null 2>&1 || {
+  echo >&2 "Expected to find Borg but it is missing. Bummer!"
+  exit 1
+}
+
+cat << __EOF__
+
+   $(bold "System is now prepared for assimilation. Type 'borg help' for info.")
+
+__EOF__
