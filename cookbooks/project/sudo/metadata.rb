13f279d0bcbc3bfd4c167569dd053e3f52e9f3fe
Alex Howells <alex@howells.me>
2014-11-03 00:50:23 +0000

    This was a horrible idea. :fire: all the way down.

--- cookbooks/project/sudo/metadata.rb
+++
@@ -1,9 +0,0 @@
-name 'sudo'
-maintainer 'Aeode Ltd'
-maintainer_email 'hello@aeode.com'
-license 'MIT'
-description 'Managing sudo on OSX so that long-running installs avoid breaking Borg.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
-
-supports 'mac_os_x'
